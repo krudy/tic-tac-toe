84694665848e93801c24fd9a47cc4c5e56c2fdb1
TheLastSpoonbender <k.rudnicki1990@gmail.com>
2023-12-11 20:42:33 +0100

    modification for OOP

--- src/index.js
+++ src/index.js
@@ -1,48 +1,116 @@
-const fieldsElements = document.querySelectorAll('.board__item');
-const panel = document.querySelector('.panel');
+class Game {
+    fields;
+    activePlayer;
+    gameActive;
 
-const fields = ['', '', '', '', '', '', '', '', ''];
+    winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [6, 4, 2]
+    ]
 
-let activePlayer = 'X';
-let gameActive = true;
+    
+    constructor() { 
+        this.board = new Board(this.handleItemClick,this.handleResetButtonClick);
+        this.setDefaults();
+    }
 
-const winningConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-]
+    validateGame = () => {
 
-const displayWinMessage = () => {
-    panel.innerText = `${activePlayer} wins!`;
-}
+        let gameWon = false;
 
-const validateGame = (winningConditions,fields,activePlayer) => {
+        for (let i = 0; i < this.winningConditions.length; i++) {
+            const [posA, posB, posC] = this.winningConditions[i];
+    
+            if (this.fields[posA] === this.activePlayer && this.fields[posB] === this.activePlayer && this.fields[posC] === this.activePlayer) {
+                gameWon = true;
+                break
+            }
+        }
+    
+        if (gameWon) {
+            this.gameActive = false;
+            this.board.displayWinMessage(this.activePlayer);
+        } else if (this.isBoardFull()) {
+            this.gameActive = false;
+            this.board.displayTieMessage();
+        }
+    
+    }
+    
+   isBoardFull = () => {
+        return this.fields.find(field => field === '') === undefined;
+    }
+    
+    
+    handleResetButtonClick = () => {
+        this.setDefaults();
+    }
+    
+    handleItemClick = (event) => {
+        console.log('test2');
+        const { position } = event.target.dataset;
+    
+        if (this.gameActive && this.fields[position] === '') {
+            this.fields[position] = this.activePlayer;
+            event.target.classList.add(`board__item--filled-${this.activePlayer}`);
+            this.validateGame();
+            this.activePlayer = this.activePlayer === 'X' ? 'O' : 'X';
+        }
+    
+    }
 
-    for(let i = 0; i < winningConditions.length ;i++){
-        const [posA, posB, posC] = winningConditions[i];
-       if (fields[posA] === activePlayer && fields[posB] === activePlayer && fields[posC] === activePlayer) {
-        gameActive = false;
-       }
+    setDefaults = () => {
+
+        this.fields = ['', '', '', '', '', '', '', '', ''];
+        this.activePlayer = 'X';
+        this.gameActive = true;
     }
 }
 
-fieldsElements.forEach(field => {
-    field.addEventListener('click', (event) => {
+class Board {
+    fieldsElements = document.querySelectorAll('.board__item');
+    panel = document.querySelector('.panel');
+    btnReset = document.querySelector('.reset-button');
 
-        const {position} = event.target.dataset;
-        
-        if(gameActive && fields[position] === ''){
-            fields[position] = activePlayer;
-            event.target.classList.add(`board__item--filled-${activePlayer}`);
-            validateGame(winningConditions,fields,activePlayer);
-            activePlayer = activePlayer === 'X' ? 'O' : 'X';
-        }
+    constructor(onItemClick, onButtonClick) {
+        this.onButtonClick = onButtonClick;
+        this.btnReset.addEventListener('click', this.handleResetButtonClick);
 
-    })
-})
+        this.fieldsElements.forEach(field => {
+            field.addEventListener('click', onItemClick)
+        })
+    }
 
+    handleResetButtonClick = () => {
+        this.clearPanelMessage();
+        this.resetBoardClasses();
+        this.onButtonClick();
+    }
+
+    resetBoardClasses = () => {
+
+        this.fieldsElements.forEach(field => {
+            field.classList.remove('board__item--filled-X', 'board__item--filled-O');
+        })
+    };
+
+    displayWinMessage = (activePlayer) => {
+        this.panel.innerText = `${activePlayer} wins!`;
+    };
+
+    displayTieMessage = () => {
+        this.panel.innerText = 'no winner emerged.';
+    };
+
+    clearPanelMessage = () => {
+        this.panel.innerText = '';
+    };
+}
+
+const game = new Game();
